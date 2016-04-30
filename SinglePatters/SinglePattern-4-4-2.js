71350aa4794707616fa92242a86c6414f96886bc
wangfeng <wangfeng@Ctrip.com>
2016-05-01 01:29:06 +0800

    改变透明模式的设计模式

--- SinglePatters/SinglePattern-4-4-2.js
+++ SinglePatters/SinglePattern-4-4-2.js
@@ -1,7 +1,7 @@
 /**
- * 
- * @authors 
- * @links 
+ *
+ * @authors
+ * @links
  * @date    2016-04-30 23:41:56
  * @version 0.0.1
  */
@@ -25,3 +25,15 @@ var CreateDiv = (function() {
 
 	return CreateDiv;
 })();
+
+var a = new CreateDiv ('sven1');
+var b = new CreateDiv ('sven2');
+
+console.log(a === b);
+
+/**
+* 为了把instance封装起来，我们使用了自执行的匿名函数和闭包，并且让这个匿名函数返回真正的
+* singleton 构造方法，这就增加了一些程序的复杂度，阅读起来也不是很舒服。
+* 观察现在的构造函数：
+*
+*/
