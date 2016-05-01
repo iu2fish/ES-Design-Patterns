e44e3a1a09589c6b7908976c1d698f03ab5577b4
wangfeng <wangfeng@Ctrip.com>
2016-05-01 21:33:26 +0800

    modified 4-4-2.js

--- SinglePatters/SinglePattern-4-4-2.js
+++ SinglePatters/SinglePattern-4-4-2.js
@@ -37,3 +37,20 @@ console.log(a === b);
 * 观察现在的构造函数：
 *
 */
+
+ var CreateDiv = function (html) {
+	 if (instance) {
+		 return instance;
+	 }
+	 this.html = html;
+	 this.init();
+	 return instance = this;
+ }
+
+ /**
+	*在这段代码中，createDiv实际做了2件事，第一，是创建对象和执行初始化init的方法。
+	*第二 是保证只有一个对象。虽然我们这里还未接触过“单一职责原则”的概念，但是可以明确的是，这是一种不好的做法，至少这个构造函数看起来很奇怪。
+
+	*假设我们某天需要利用这个类，在页面中创建很多个div，即要让这个类从单例变成一个普通的可以产生多个实例的类，那我们必须得写createDiv构造函数，
+	*把控制创建唯一对象的那一段去掉，这种修改会给我们带来不必要的麻烦。
+ */
